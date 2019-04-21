e869bd8dbc0ead3b66d10141c68e9c1c95081ecf
mkurayan <maximkurayan@gmail.com>
2019-04-21 12:26:50 +0300

    606. Construct String from Binary Tree.
    1021. Remove Outermost Parentheses.

---
+++ 1021. Remove Outermost Parentheses.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string RemoveOuterParentheses(string S)
+    {
+        if (S == null || S.Length == 0)
+        {
+            return "";
+        }
+
+        StringBuilder decomposition = new StringBuilder();
+
+        int counter = 0;
+        for (int i = 0; i < S.Length; i++)
+        {
+            if (S[i] == '(')
+            {
+                counter++;
+
+                if (counter > 1)
+                {
+                    decomposition.Append(S[i]);
+                }
+            }
+            else
+            {
+                counter--;
+
+                if (counter > 0)
+                {
+                    decomposition.Append(S[i]);
+                }
+            }
+        }
+
+        return decomposition.ToString();
+    }
+}
---
+++ 606. Construct String from Binary Tree.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+using System.Text;
+
+public class Solution
+{
+    public string Tree2str(TreeNode t)
+    {
+        return traverse(t);
+
+    }
+
+    private string traverse(TreeNode t)
+    {
+        if (t == null)
+        {
+            return "";
+        }
+
+        StringBuilder st = new StringBuilder(t.val.ToString());
+
+        if (t.left != null || t.right != null)
+        {
+            st.AppendFormat("({0})", traverse(t.left));
+
+            if (t.right != null)
+            {
+                st.AppendFormat("({0})", traverse(t.right));
+            }
+
+        }
+
+        return st.ToString();
+    }
+}
