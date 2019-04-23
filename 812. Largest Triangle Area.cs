cb373db59672ec9f3c9aa26792d8a5091511ca08
mkurayan <maximkurayan@gmail.com>
2019-04-23 12:35:32 +0300

    812. Largest Triangle Area

---
+++ 812. Largest Triangle Area.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Solution
+{
+    public double LargestTriangleArea(int[][] points)
+    {
+        double maxArea = 0;
+        for (int i = 0; i < points.Length; i++)
+        {
+            for (int j = i + 1; j < points.Length; j++)
+            {
+                for (int z = j + 1; z < points.Length; z++)
+                {
+                    // Shoelace formula
+                    double area = Math.Abs(0.5 * ((points[j][0] - points[i][0]) * (points[z][1] - points[i][1]) - (points[z][0] - points[i][0]) * (points[j][1] - points[i][1])));
+
+                    if (area > maxArea)
+                    {
+                        maxArea = area;
+                    }
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
