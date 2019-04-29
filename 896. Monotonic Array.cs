b8e9a770c07b78bd86baa130d125e16ba6356cbc
mkurayan <maximkurayan@gmail.com>
2019-04-29 14:24:40 +0300

    896. Monotonic Array

---
+++ 896. Monotonic Array.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public bool IsMonotonic(int[] A)
+    {
+        if (A.Length < 2) return true;
+
+        bool decreasing = false;
+        bool increasing = false;
+
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (A[i] == A[i - 1])
+            {
+                continue;
+            }
+
+            if (A[i] > A[i - 1])
+            {
+                increasing = true;
+
+                if (decreasing)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                decreasing = true;
+
+                if (increasing)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
