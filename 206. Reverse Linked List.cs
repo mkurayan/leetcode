306348fb6da838e2fcd0a1673beed6d6b3f83787
mkurayan <maximkurayan@gmail.com>
2019-05-13 15:09:54 +0300

    206. Reverse Linked List

---
+++ 206. Reverse Linked List.cs
@@ -0,0 +1,56 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class SolutionRecursive
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        if (head.next != null)
+        {
+            ListNode last = ReverseList(head.next);
+
+            ListNode next = head.next;
+            head.next = null;
+            next.next = head;
+
+            return last;
+        }
+
+        return head;
+    }
+}
+
+public class SolutionIterative
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null)
+        {
+            next = current.next;
+            current.next = prev;
+
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
