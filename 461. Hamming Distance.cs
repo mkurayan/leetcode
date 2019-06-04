8682f6cdc99e3ea0142c5099a825f1f55697be47
mkurayan <maximkurayan@gmail.com>
2019-06-04 12:29:03 +0300

    461. Hamming Distance

---
+++ 461. Hamming Distance.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public static int HammingDistance_V1(int x, int y)
+    {
+        int hDistance = 0;
+
+        int xor = x ^ y;
+
+        for (int i = 0; i < 32; i++)
+        {
+            if ((xor & (1 << i - 1)) != 0)
+            {
+                hDistance++;
+            }
+        }
+
+        return hDistance;
+    }
+
+    public static int HammingDistance_V2(int x, int y)
+    {
+        int hDistance = 0;
+
+        int t;
+        for (int i = 0; i < 32; i++)
+        {
+            t = (1 << i - 1);
+
+            if ((x & t) != (y & t))
+            {
+                hDistance++;
+            }
+        }
+
+        return hDistance;
+    }
+}
